1a7a14b6a237e92620e7f758c294608b2df80468
sahoomohit <97326980+sahoomohit@users.noreply.github.com>
2022-01-08 09:56:12 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,29 @@
+var box;
+
+
+function setup() {
+  createCanvas(400, 400);
+  box=createSprite(200,200,50,50)
+}
+
+function draw() {
+  background(0);
+
+  if(keyIsDown(RIGHT_ARROW)){
+    box.position.x=box.position.x + 10;
+  }
+
+  if(keyIsDown(LEFT_ARROW)){
+    box.position.x=box.position.x - 10;
+  }
+
+  if(keyIsDown(UP_ARROW)){
+    box.position.y=box.position.y - 10;
+  }
+
+  if(keyIsDown(DOWN_ARROW)){
+    box.position.y=box.position.y + 10;
+  }
+
+  drawSprites();
+}
